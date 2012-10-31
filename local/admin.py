0b8f9f26960c95e22a785ee639a25b3c7728a904
Irving Kcam <xploit29@gmail.com>
2012-10-30 23:20:28 -0500

    Ver estado de la mesa

--- local/admin.py
+++ local/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from local.models import Zona
 from local.models import Mesa
 from local.models import Cliente
+from ventas.models import Comanda
 
 class ZonaAdmin(admin.ModelAdmin):
 	list_display = ('nombre', 'descripcion')
@@ -10,7 +11,12 @@ class MesaAdmin(admin.ModelAdmin):
 	list_display = ('nombre', 'capacidad', 'zona', 'disponible', 'encargado')
 
 	def disponible(self,obj):
-		return False
+		Comandas = Comanda.objects.filter(mesa=obj.id,activa=True).count()
+		if Comandas > 0:
+			return 'Ocupada'
+		else:
+			return 'Disponible'
+
 
 	def encargado(self,obj):
 		return "Alguien"
